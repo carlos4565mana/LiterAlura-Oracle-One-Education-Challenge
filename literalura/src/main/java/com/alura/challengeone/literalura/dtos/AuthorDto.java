b87a37a5d28f43cc94648d0a7bbed3bd2ba7dd2e
carlos4565mana <carloscal61@gmail.com>
2024-05-17 12:50:01 -0300

    Organizing the project into packages, creating the AuthorDto, BookDto classes

---
+++ literalura/src/main/java/com/alura/challengeone/literalura/dtos/AuthorDto.java
@@ -0,0 +1,8 @@
+package com.alura.challengeone.literalura.dtos;
+
+public record AuthorDto(
+        String name,
+        Integer birthYear,
+        Integer deathYear
+) {
+}
---
+++ literalura/src/main/java/com/alura/challengeone/literalura/dtos/BookDto.java
@@ -0,0 +1,17 @@
+package com.alura.challengeone.literalura.dtos;
+
+import java.util.List;
+import java.util.Map;
+
+public record BookDto(
+        int id,
+       List<AuthorDto> authors,
+        List<String> subjects,
+        List<String> languages,
+        boolean copyright,
+        String mediaType,
+        Map<String, String> formats,
+        Integer downloadCount
+
+) {
+}
