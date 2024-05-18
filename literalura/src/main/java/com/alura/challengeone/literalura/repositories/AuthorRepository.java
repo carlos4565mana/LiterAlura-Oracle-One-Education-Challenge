6ae3223a63e53b049976ead8c28a23489c281e05
carlos4565mana <carloscal61@gmail.com>
2024-05-17 21:02:55 -0300

    implemented the save, saveAll and get methods in the class AuthorService

--- literalura/src/main/java/com/alura/challengeone/literalura/repositories/AuthorRepository.java
+++ literalura/src/main/java/com/alura/challengeone/literalura/repositories/AuthorRepository.java
@@ -3,5 +3,8 @@ package com.alura.challengeone.literalura.repositories;
 import com.alura.challengeone.literalura.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository  extends JpaRepository<Author, Long> {
+    Optional<Author> findByName(String name);
 }
--- literalura/src/main/java/com/alura/challengeone/literalura/services/AuthorService.java
+++ literalura/src/main/java/com/alura/challengeone/literalura/services/AuthorService.java
@@ -1,15 +1,52 @@
 package com.alura.challengeone.literalura.services;
 
+import com.alura.challengeone.literalura.converter.AuthorConverter;
 import com.alura.challengeone.literalura.dtos.AuthorDto;
 import com.alura.challengeone.literalura.entities.Author;
+import com.alura.challengeone.literalura.repositories.AuthorRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
+
+    private AuthorRepository  repository;
+
+    public AuthorService(AuthorRepository repository) {
+        this.repository = repository;
+    }
+
     public Set<Author> saveAll(HashSet<AuthorDto> authorDtos) {
+        Set<Author> authors = authorDtos
+                .stream()
+                .map(authorDto -> this.save(AuthorConverter.converterFromDto(authorDto)))
+                .collect(Collectors.toSet());
+        return authors;
+
+    }
+
+    public Author save(Author author) {
+       if(!this.get(author.getName()).isEmpty()) return this.get(author.getName()).get();
+       return repository.save((author));
+    }
+
+    public  Optional<Author> get(String name) {
+       return repository.findByName(name);
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
